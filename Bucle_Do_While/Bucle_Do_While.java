d4d524aa723a37371c8e5832a5a4f03d18334099
SergioRosas28 <sergio.rosasr@campusucc.edu.co>
2025-11-09 20:09:55 +0000

    Bucle while

---
+++ Bucle_Do_While/Bucle_Do_While.java
@@ -0,0 +1,24 @@
+package Bucle_Do_While;
+
+import java.util.Scanner;
+
+public class Bucle_Do_While {
+    public static void main(String[] args) {    
+    int contador = 1;
+    final var valor = 5;
+    Scanner entrada = new Scanner(System.in); 
+    //do{
+        /*System.out.println("Valor: " + contador ++);
+    }while(contador<= valor); 
+    System.out.println("Fin del bucle");
+*/
+      int numero = 0;
+      do{
+        System.out.println("Ingrese el numero 5...");
+        numero = entrada.nextInt();
+        if(numero!=5);
+        System.out.println("No ingresaste el numero 5");
+      }while(numero!= 5);
+      System.out.println("Gracias");
+    }
+ }
