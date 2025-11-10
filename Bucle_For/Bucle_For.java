3f025a1d059f1efdc388c7a28c2787f4e25a7444
SergioRosas28 <sergio.rosasr@campusucc.edu.co>
2025-11-10 21:17:36 +0000

    Bucle_For

--- Bucle_For/Bucle_For.java
+++ Bucle_For/Bucle_For.java
@@ -1,10 +1,17 @@
 public class Bucle_For {
 
     public static void main(String[] args) {
-        
-        
-    }
 
+         for (int i = 0; i <= 10; i++) {
+         System.out.println(i);
+         System.out.println("Fin Del Bucle");
+        for (int i = 100; i >= 0; i--) {
+            System.out.println("Numero de vuelta: "+ i);
+        
+        int suma = 0;
+        for(int i = 0; i<= 20; i ++){
+            
+        }    
+        }
     }
-    
-}
+    }
